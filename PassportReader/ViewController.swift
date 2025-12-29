26ccd8d4a83bc35ad7bbaa0c752fcf0fc7278469
YUFOHON <yukehan8@gmail.com>
2025-12-29 17:04:44 +0800

    Initial Commit

---
+++ PassportReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassportReader
+//
+//  Created by Fo Hon Yu on 29/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
